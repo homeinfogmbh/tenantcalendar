78bfacce0546db8b4c723ecd123f7c50eab3e498
Richard Neumann <mail@richard-neumann.de>
2021-09-28 13:07:19 +0200

    Export ERRORs and MissingContactInfo

--- tenantcalendar/__init__.py
+++ tenantcalendar/__init__.py
@@ -1,7 +1,9 @@
 """Tenant calendar for ComCat."""
 
+from tenantcalendar.errors import ERRORS
+from tenantcalendar.exceptions import MissingContactInfo
 from tenantcalendar.functions import list_events, get_event
 from tenantcalendar.orm import Event
 
 
-__all__ = ['Event', 'list_events', 'get_event']
+__all__ = ['ERRORS', 'MissingContactInfo', 'Event', 'list_events', 'get_event']
