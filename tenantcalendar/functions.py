b70018268339cf1409ab75a60b4dbf5ab682752f
Richard Neumann <mail@richard-neumann.de>
2021-09-28 13:14:09 +0200

    Add optional start and end kwargs

--- tenantcalendar/functions.py
+++ tenantcalendar/functions.py
@@ -1,5 +1,8 @@
 """Common functions."""
 
+from datetime import datetime
+from typing import Optional
+
 from peewee import ModelSelect
 
 from comcatlib import User
@@ -11,11 +14,19 @@ from tenantcalendar.orm import Event
 __all__ = ['list_events', 'get_event']
 
 
-def list_events(customer: Customer) -> ModelSelect:
+def list_events(customer: Customer, *, start: Optional[datetime] = None,
+                end: Optional[datetime] = None) -> ModelSelect:
     """Lists events of a customer."""
 
-    return Event.select().join(User).join(Tenement).where(
-        Tenement.customer == customer)
+    condition = Tenement.customer == customer
+
+    if start is not None:
+        condition &= Event.start >= start
+
+    if end is not None:
+        condition &= Event.end < end
+
+    return Event.select().join(User).join(Tenement).where(condition)
 
 
 def get_event(ident: int, customer: Customer) -> Event:
