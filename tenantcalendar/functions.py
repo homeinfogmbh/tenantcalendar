ba8ff520ba64597c6784d492fc6c209d4fe2b3ca
Richard Neumann <r.neumann@homeinfo.de>
2021-11-19 14:11:27 +0100

    Add lising of own events

--- tenantcalendar/functions.py
+++ tenantcalendar/functions.py
@@ -11,7 +11,14 @@ from mdb import Company, Customer, Tenement
 from tenantcalendar.orm import CustomerEvent, UserEvent
 
 
-__all__ = ['list_user_events', 'get_user_event']
+__all__ = [
+    'list_customer_events',
+    'get_customer_event',
+    'list_user_events',
+    'get_user_event',
+    'list_own_events',
+    'get_own_event'
+]
 
 
 def list_customer_events(customer: Customer, *,
@@ -57,3 +64,24 @@ def get_user_event(ident: int, customer: Customer) -> UserEvent:
     """Returns the given event of a customer."""
 
     return list_user_events(customer).where(UserEvent.id == ident).get()
+
+
+def list_own_events(user: User, *, start: Optional[datetime] = None,
+                    end: Optional[datetime] = None) -> ModelSelect:
+    """Lists user events."""
+
+    condition = UserEvent.user == user
+
+    if start is not None:
+        condition &= UserEvent.start >= start
+
+    if end is not None:
+        condition &= UserEvent.end < end
+
+    return UserEvent.select().join(User).join(Tenement).where(condition)
+
+
+def get_own_event(ident: int, user: User) -> UserEvent:
+    """Returns the given event of a particular user."""
+
+    return list_own_events(user).where(UserEvent.id == ident).get()
